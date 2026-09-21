c4a72d87dd9d2eaa3122156660614859e45d5383
Aditya <adityakumarchursiya07@gmail.com>
2026-09-22 02:13:55 +0530

    hw 1 completed

---
+++ hw1.js
@@ -0,0 +1,613 @@
+//Js hW 1
+// Q 1
+// let a = 10;
+// let b = 20;
+// console.log(a+b);
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+
+//Q 2 swapping variable
+//  let temp = a;
+// a= b; 
+// b = temp;
+// console.log(a)
+// console.log(b)
+
+//Q no. 3 
+// let subject1 = 80; 
+// let subject2 = 54; 
+// let subject3 = 89;
+// let totalMraks = subject1 +subject2+ subject3;
+// let averageMarks = totalMraks/3; 
+// let percentage= (totalMraks /3 )*100;
+// console.log("the total marks of subject is ",totalMraks);
+// console.log("the average marks of subjects is ",averageMarks);
+// console.log("the percentage is ",percentage);
+
+
+//Q no. 4 
+// let productPrice = 100 ;
+// let quantity = 5;
+// let origninalprice = productPrice*quantity;
+// let discount = origninalprice / 10 ;
+// let finlalBill = origninalprice - discount;
+// console.log("the original price is =", origninalprice);
+// console.log("the discount is 10 percentage is =",discount);
+// console.log("the final bill is =",finlalBill);
+
+
+
+// //Q no. 5
+// let l = 5; 
+// let w = 10 ; 
+// let areaFormula = l*w;
+// let araeperimeter = 2*(l+w);
+// console.log("Area of rectangle is ", areaFormula);
+// console.log("Area of perimeter is ", araeperimeter);
+
+
+
+//Q no. 6
+// let num = 21;
+// if(num %2 == 0 ){
+//     console.log("the number is even ", num );
+// }
+// else console.log("the number is odd ", num);
+
+
+//Q no. 7
+//   let num = 0;
+//   if ( num < 0 ){
+//     console.log("the number is negative", num );
+//   }
+//   else if ( num > 0 ) {
+//     console.log("the number is positve ", num );
+//   }
+//   else console.log("the num is zero");
+
+//Q no. 8
+// let num1 = 10;
+// let num2 = 10;
+//   if ( num1 < num2 ){
+//     console.log("the number is greater", num2 );
+//   }
+//   else if ( num1 > num2  ) {
+//     console.log("the number is greaater ", num1 );
+//   }
+//   else console.log("the num is equal "  ,num1 , num2);
+
+//Q no. 8
+
+// let num1 = 10;
+// let num2 = 10;
+// let num3 = 10;
+//   if ( num1 < num2 ){
+//     console.log("the number is greater", num2 );
+//   }
+//   else if ( num2 > num3  ) {
+//     console.log("the number is greaater ", num1 );
+//   }
+//   else if ( num3 > num1  ) {
+//     console.log("the number is greaater ", num3 );
+//   }
+//   else console.log("the num is equal "  ,num1 );
+
+// Q10 voting 
+
+// let age = 15 ;
+// if (age >= 18 ){
+//     console.log("the person is eligible for driveing ");
+
+// }
+// else console.log("the person is not  eligible for driveing");
+
+
+//Q no 12
+
+// let age = 15 ;
+
+// if (age >= 18 ){
+//     console.log("the person is eligible for driveing ");
+
+// }
+// else console.log("the person is not  eligible for driveing");
+
+
+
+// //Q no 12
+// let num = 55 ;
+
+// let i = 10 ; 
+//      if (num <= 10 && num>=100){
+//         console.log("the num is between 10 to 100"); 
+//     }
+//     else console.log("the num is  not between 10 to 100");
+
+
+// // //Q no 13
+// let p = 28;
+// if (p <= 100 && p >=90 ){
+//     console.log("Your grade is A ");
+// }
+// else if (p <= 89 && p >=80){
+//       console.log("Your grade is B");
+// }
+// else if (p <= 79 && p >=70){
+//       console.log("Your grade is C");
+// }
+// else if (p <= 69 && p >=60){
+//       console.log("Your grade is D");
+// }
+// else if (p <=59&& p >=48){
+//       console.log("Your grade is E");
+// }
+// else if ( p <=39 && p >= 0){
+//     console.log("FAIL");
+// }
+// else console.log("wrong number");
+
+
+
+
+
+
+
+
+
+
+// // //Q no 14
+
+// let math =56 ;
+// let sceince =56;
+// let physics = 59;
+
+// let total = math + sceince + physics;
+// let average = total/3;
+//  if (math < 40 || sceince < 40 || physics < 40 ){
+//     console.log("FAIL");
+//  }
+//  else {
+//     if ( average >= 75 ){
+//         console.log(" Distinction");
+//     }
+//     else if (average >= 60){
+//         console.log(" First Division");
+//     }
+//     else if (average >= 50){
+//         console.log(" Second Division");
+//     }
+//     else {
+//         console.log(" PASS");
+//     }
+//  }
+
+
+// // // //Q no 15
+// let units = 201;
+// let finalBill = 0 
+// if (units >= 0 && units <=100){
+//    finalBill= units * 5 ;
+// }
+// else if ( units >= 101 && units <=200 ){
+//    finalBill= units * 7;
+// }
+
+// else {
+// finalBill = (100 * 5 ) + (100 * 7 ) + ((units - 200 )*10);
+// } 
+// console.log(finalBill);
+
+
+
+// // // //Q no 16
+// let username;
+// let password;
+
+// username = "admin";
+// password = "123456";
+
+// if(username === "admin" && password ==="12345"){
+//     console.log("Login successful");
+// }
+// else console.log("Invalid username or password");
+
+
+// // //Q no 17
+// let salary = 25000;
+// let experince = 5;
+// let originalSalary = salary;
+// function salaryDislay() {
+//    finalSalary = originalSalary + bonus;
+//    console.log("original salary = ", `${salary}`);
+//    console.log("your bonus is =", `${bonus}`);
+//    console.log("Final salaary is =", `${finalSalary}`)
+// }
+// // Value = Total × 0.20
+
+// if (experince >= 10) {
+//    bonus = salary * 0.20;
+
+//    salaryDislay();
+// }
+
+// else if (experince >= 5) {
+//    bonus = salary * 0.10;
+//    salaryDislay();
+// }
+
+// else {
+//    bonus = salary * 0.05;
+//    salaryDislay();
+// }
+
+//Q no 17 age category 
+// let age = 15;
+// if (age >=0 && age <=12){
+//     console.log("child");
+// }
+// else if (age >=13 && age <=19){
+//     console.log(" Teenager");
+// }
+// else if (age >=20 && age <=59){
+//     console.log(" Adult");
+// }
+// else {
+//     console.log(" Senior Citizen");
+// }
+
+//Q no   19. Day of the Week
+// let day = 6;
+// switch (day){
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log(" Wednesday");
+//         break;
+//     case 4:
+//         console.log(" Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     case 7:
+//         console.log(" Sunday");
+//         break;
+//         default: console.log("INVALID DAY ");
+// }
+
+// //Q no 20.  Simple Calculator
+// let firstNumber = 20;
+// let secondNumber = 20 ;
+// let operator= "/";
+// switch (operator){
+//     case "+":
+//         console.log(firstNumber + secondNumber);
+//         break;
+//     case  "-":
+//         console.log(firstNumber - secondNumber);
+//         break;
+//     case  "*":
+//         console.log(firstNumber * secondNumber);
+//         break;
+//     case  "/":
+//         if (firstNumber ===0 || secondNumber===0 ) {
+//             console.log("0");
+//         }
+//         else console.log(firstNumber / firstNumber);
+//         break;
+//     case "%":
+//         console.log(firstNumber % secondNumber);
+//         break;
+//         default: console.log("Wrong input ");
+// }
+
+// // //Q no 21.  Month Name
+// let month = 7;
+
+// switch (month){
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log(" February");
+//         break;
+//     case 3:
+//         console.log(" March");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+//     case 5:
+//         console.log("May");
+//         break;
+//     case 6:
+//         console.log("June");
+//         break;
+//     case 7:
+//         console.log("july");
+//         break;
+//     case 8:
+//         console.log("Aungust");
+//         break;
+//     case 9:
+//         console.log("September");
+//         break;
+//     case 10:
+//         console.log("October");
+//         break;
+//     case 11:
+//         console.log("Novmber");
+//         break;
+//     case 12:
+//         console.log("December");
+//         break;
+//         default: console.log("INVALID DAY ");
+// }
+
+
+
+// Q no 20.  Simple Calculator
+// let a = 20;
+// let b = 30 ;
+// let operator= "Subtraction";
+// switch (operator){
+//     case " Addition":
+//         console.log(a + b);
+//         break;
+//     case  "Subtraction":
+//         console.log(a - b);
+//         break;
+//     case  "Multiplication":
+//         console.log(a * b);
+//         break;
+//     case  "Division":
+//         if (a ===0 || b===0 ) {
+//             console.log("0");
+//         }
+//         else console.log(a / b);
+//         break;
+//     case "Modulus":
+//         console.log(a % b);
+//         break;
+//         default: console.log("Wrong input ");
+// }
+
+// Q no 23. Traffic Signal
+
+// let trafficRules ="Yellow" ;
+
+// switch (trafficRules){
+//     case "red":
+//        console.log("stop");
+       
+//         break;
+//     case "Yellow":
+//           console.log("Wait");
+//         break;
+//     case  "Green":
+//         console.log("Go");     
+//         break;
+
+//         default: console.log("Invalid signal");
+// }
+
+
+
+
+// Q no 24. ATM Transaction
+
+// let balance = 10000;
+// let withdrawAmount = 0;
+
+// if (withdrawAmount >= 0 ){
+//     console.log("Withdrawal amount must be greater than 0.");
+// }
+// else if (withdrawAmount > balance){
+//     console.log("Withdrawal amount is  greater than the balance");
+// }
+// else if ( withdrawAmount <= balance && withdrawAmount <=balance){
+//     console.log("Withdrawal successful");
+//     console.log("Remaining balance:", `${balance-withdrawAmount}`);
+// }
+
+
+// Q no 25.  Movie Ticket System
+// let Age = 70;
+// let numberOfTickets = 7;
+// let price = 100;
+
+
+// if (Age <= 12 ){
+   
+//     console.log( "Age",Age);
+//     console.log("total : ₹",`${ price * numberOfTickets}`);
+// }
+// else if (Age <=13 || Age <=59){
+   
+//     console.log("Age",Age);
+//     console.log("total : ₹",`${ price * 2 *numberOfTickets}`);
+// }
+
+// else if (Age >=60) {
+//     price = 120 ;
+//      console.log("Age",Age);
+//      console.log("total : ₹",`${ price*numberOfTickets}`);
+// }
+
+
+
+//  Q no 26.Restaurant Menu
+// let choice = 1 ;
+// let quantity = 3;
+// let burger = 150 ;
+// let pizza = 250 ;
+// let pasta = 180 ;
+// let sandwich = 120 ;
+
+// switch (choice){
+//     case 1 :
+//           console.log(`"choice ,"${choice} "Qunatity"${quantity}`);
+//         console.log("total",`${ burger*quantity }`);
+//         break;
+//     case 2:
+//              console.log(`"choice ",${choice} "Qunatity",${quantity}`);
+//         console.log("total",`${pizza*quantity }`);
+//         break;
+//     case 3:
+//          console.log(`"choice ,"${choice} "Qunatity"${quantity}`);
+//         console.log("total",`${ pasta*quantity }`);
+//         break;
+//     case  4:
+//             console.log(`"choice ,"${choice} "Qunatity"${quantity}`);
+//         console.log("total",`${ sandwich*quantity }`);
+//         break;
+
+//         default: console.log("Invalid  menu option");
+// }
+
+
+
+// //  Q no 27.Electricity Bill + Discount
+
+
+// let units = 501;
+// let finalBill = 0 
+// if (units >= 0 && units <=100){
+//    finalBill= units * 5 ;
+
+// }
+// else if ( units >= 101 && units <=200 ){
+//    finalBill= units * 7;
+// }
+
+// else {
+// finalBill = (100 * 5 ) + (100 * 7 ) + ((units - 200 )*10);
+// } 
+// let disconut;
+// if (finalBill >= 2000 ){
+//     disconut = finalBill * 10/100;
+     
+// }
+// else {
+//     disconut = 0;
+// }
+// let originalbill = finalBill-disconut;
+// console.log("units :" ,`${units}`);
+// console.log("original bill :" ,`${originalbill}`);
+// console.log("disconut :" ,`${  disconut}`);
+// console.log("final bill :" ,`${finalBill}`);
+
+
+
+// //  Q no 28 Simple ATM Menu
+
+// let balance = 500000000;
+// let deposit = 500000000;
+// let Withdraw = 50000;
+// let menu = "Withdraw"
+
+// switch (menu){
+//     case "check balance" :
+//         console.log(balance);
+//         break;
+//     case "Deposit":
+//             console.log( balance + deposit);  
+//         break;
+//     case "Withdraw":
+//          if (balance > Withdraw){
+//             console.log("sufficient balance exists",`${balance}`);
+//          }
+//          else console.log(" Not sufficient balance exists");
+//          break;
+//     case "exit":
+//          console.log("M  bank ko loan dedu ");////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//         break;
+//     case  4:
+            
+//         break;
+
+//         default: console.log("Error");
+// }
+
+// // //  Q no 29 Number Classification
+// let n = 104;
+
+// if ( n < 0 ) console.log("negative");
+// else if (n ===0 ) console.log("zero");
+// else  console.log("positive");
+
+// if (n%2 === 0 ){ 
+//     console.log("Even");
+//     }
+// else console.log("odd");
+
+// if(n < 100) console.log("less than 100");
+// else if (n > 100 ) console.log(" greater than 100");
+// else console.log("equal to 100");
+
+
+
+
+// // //  Q no 29 Number Classification
+ let Name = "Aditya"
+ let rollnumber = "998"
+ let mathMarks = 85; 
+let scienceMarks = 78; 
+let englishMarks = 92;
+let totalMraks = mathMarks +scienceMarks+ englishMarks;
+let averageMarks = totalMraks/3; 
+let percentage= (totalMraks /3 )*100;
+let grade ;
+let result;
+
+// console.log("the average marks of subjects is ",averageMarks);
+
+ let p = (totalMraks /3 );
+if (p <= 100 && p >=90 ){
+    grade = "Your grade is A ";
+    
+}
+else if (p <= 89 && p >=80){
+    grade = "Your grade is B";
+     
+}
+else if (p <= 79 && p >=70){
+ grade = "Your grade is C";
+}
+else if (p <= 69 && p >=60){
+ grade = "Your grade is D";
+}
+else if (p <=59&& p >=48){
+      grade = "Your grade is E";
+}
+else if ( p <=39 && p >= 0){
+     grade = "Your grade is F";
+}
+else console.log("wrong number");
+
+if ( mathMarks  >= 40 && scienceMarks >= 40 && englishMarks >= 40  ) {
+    result = "pass";
+}
+else result = "fail"
+
+
+console.log("Name:",`${Name}`);
+console.log("Roll numer:",`${rollnumber}`);
+console.log("Math:",`${mathMarks}`);
+console.log("science:",`${scienceMarks}`);
+console.log("English:",`${englishMarks}`);
+console.log("the total marks of subject is : ",totalMraks);
+console.log("the percentage is : ",p,("%"));
+console.log("the Greade is :  ",grade);
+console.log("the result is : ",result);
+
+
+
